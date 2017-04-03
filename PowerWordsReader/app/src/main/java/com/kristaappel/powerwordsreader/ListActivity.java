1ec3f98ccc481b566a40850a0c7772f015615ad2
kristaappel <mrskrista85@gmail.com>
2017-04-03 10:13:03 -0400

    Project Creation

---
+++ PowerWordsReader/app/src/main/java/com/kristaappel/powerwordsreader/ListActivity.java
@@ -0,0 +1,17 @@
+// Krista Appel
+// Application Deployment 2: Andorid
+// ListActivity.java
+
+package com.kristaappel.powerwordsreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
