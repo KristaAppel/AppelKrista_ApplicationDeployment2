06300fcb779510cc989c030c25b051f37fc6abdc
kristaappel <mrskrista85@gmail.com>
2017-04-25 10:48:23 -0400

    Progress Log screen now displays saved data

--- PowerWordsReader/app/src/main/java/com/kristaappel/powerwordsreader/ProgressLogActivity.java
+++ PowerWordsReader/app/src/main/java/com/kristaappel/powerwordsreader/ProgressLogActivity.java
@@ -7,12 +7,17 @@ package com.kristaappel.powerwordsreader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.kristaappel.powerwordsreader.fragments.LogListFrag;
+
 public class ProgressLogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_log);
+
+        LogListFrag logListFrag = LogListFrag.newInstance();
+        getFragmentManager().beginTransaction().replace(R.id.log_frame, logListFrag).commit();
     }
 
 
---
+++ PowerWordsReader/app/src/main/java/com/kristaappel/powerwordsreader/fragments/LogListFrag.java
@@ -0,0 +1,86 @@
+// Krista Appel
+// Application Deployment 2: Android
+// LogListFrag.java
+
+package com.kristaappel.powerwordsreader.fragments;
+
+import android.app.ListFragment;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.kristaappel.powerwordsreader.R;
+import com.kristaappel.powerwordsreader.objects.FileUtil;
+import com.kristaappel.powerwordsreader.objects.Score;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+
+public class LogListFrag extends ListFragment {
+
+    ArrayList<Score> scores;
+    private static final int ID_CONSTANT = 0x02020;
+
+    public static LogListFrag newInstance(){
+        return new LogListFrag();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        scores = FileUtil.read(getActivity());
+        setListAdapter(new ListAdapter());
+    }
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        // TODO - do something?
+    }
+
+
+    private class ListAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return scores.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return scores.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return ID_CONSTANT + position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.log_list_item, parent, false);
+            }
+
+            TextView textDate = (TextView) convertView.findViewById(R.id.textView_log_date);
+            TextView textAccuracy = (TextView) convertView.findViewById(R.id.textView_log_accuracy);
+
+            // Display the dates and scores in reverse for chronological order:
+            textDate.setText(scores.get(getCount() -1 - position).getTime());
+            textAccuracy.setText(scores.get(getCount() -1 - position).getScore());
+
+            return convertView;
+        }
+    }
+
+
+}
